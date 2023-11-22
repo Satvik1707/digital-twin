bf1eb89dcbd53f4ae742c2214671dd7ef1f56e74
Satvik1707 <satvik0125@gmail.com>
2023-11-22 16:22:23 +0530

    2

--- flask-server/app.py
+++ flask-server/app.py
@@ -1,12 +1,23 @@
-from flask import Flask
+from flask import Flask, jsonify
+from flask_cors import CORS
+import subprocess
 import pickle
 import numpy as np
 
 app = Flask(__name__)
+CORS(app, resources={r"/lr-model/*": {"origins": "http://localhost:3000"}})
 
-@app.route('/model-1', methods=['GET'])
-def model_1():
-    return "Model 1"
+@app.route('/lr-model/<float:avg_connected_ue_value>', methods=['GET'])
+def model_1(avg_connected_ue_value):
+    try:
+        result = subprocess.run(['python', 'models/Model-1/LR.py', str(avg_connected_ue_value)], capture_output=True, text=True)
+        
+        if result.returncode == 0:
+            return jsonify({'result': result.stdout})
+        else:
+            return jsonify({'error': result.stderr}), 500
+    except Exception as e:
+        return jsonify({'error': str(e)}), 500
 
 @app.route('/model-2', methods=['GET'])
 def model_2():
